c7f58a3f9ae3f65788ff16a3e01c2541e47e7fba
Aditya Maulana <slenkoid@gmail.com>
2018-04-17 17:31:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://res.cloudinary.com/spazeeid/image/upload/lhukm16yizg5dl58e2vi" style={{display:'block', marginLeft:'auto', marginRight:'auto', marginTop: '15%'}} alt="logo"/>
+      </div>
+    );
+  }
+}
+
+export default App;
